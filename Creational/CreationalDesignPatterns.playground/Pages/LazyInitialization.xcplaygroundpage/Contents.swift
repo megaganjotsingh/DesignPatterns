8a6b5633aa8355bef10dc58a1b5ec27d35284743
Gaganjot singh <megaganjotsingh@gmail.com>
2023-10-11 17:59:57 +0530

    example added

--- Creational/CreationalDesignPatterns.playground/Pages/LazyInitialization.xcplaygroundpage/Contents.swift
+++ Creational/CreationalDesignPatterns.playground/Pages/LazyInitialization.xcplaygroundpage/Contents.swift
@@ -1,7 +1,44 @@
-//: [Previous](@previous)
+class SleepingBeauty {
+    init() {
+        print("zzzz... Sleeping")
+        sleep(2)
+        print("Awaken")
+    }
+}
 
-import Foundation
+class DataManager {
+    
+//    var sleepingBeauty = SleepingBeauty()
+    lazy var sleepingBeauty = SleepingBeauty()
+    
+    init() {
+        print("initialized DataManager")
+    }
+}
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+//print("Start")
+//
+//let dataManager = DataManager()
+
+//// // without lazy with SleepingBeauty instance in DataManager class
+//  OUTPUT --------------------------------------
+//Start
+//zzzz... Sleeping
+//Awaken
+//initialized DataManager
+
+print("Start")
+let dataManager = DataManager()
+dataManager.sleepingBeauty
+
+//// // lazy with SleepingBeauty instance in DataManager class
+//  OUTPUT --------------------------------------
+
+//Start
+//initialized DataManager
+//zzzz... Sleeping
+//Awaken
+
+
+
