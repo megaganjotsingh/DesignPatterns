69eaa4a3223d5f54b2b1ffc48f24c4b2117fb72f
Gaganjot singh <megaganjotsingh@gmail.com>
2023-10-15 22:46:54 +0530

    Example added

--- Creational/CreationalDesignPatterns.playground/Pages/Prototype.xcplaygroundpage/Contents.swift
+++ Creational/CreationalDesignPatterns.playground/Pages/Prototype.xcplaygroundpage/Contents.swift
@@ -1,7 +1,39 @@
-//: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var greeting = "Hello, playground"
+// Step 1: Create a protocol that defines the clone method
+protocol UserClonable {
+  func clone() -> Self
+}
 
-//: [Next](@next)
+// Step 2: Create a concrete class that adopts the clonable protocol
+final class User: UserClonable {
+  private(set) var username: String
+  private(set) var bio: String
+
+  init(username: String, bio: String) {
+    self.username = username
+    self.bio = bio
+  }
+
+  // Step 3: Implement the clone method to create a copy of the object
+  func clone() -> User {
+    let clone = User(username: self.username, bio: self.bio)
+    return clone
+  }
+
+  func customize(username: String, bio: String) -> User {
+    let clone = self.clone()
+    clone.username = username
+    clone.bio = bio
+    return clone
+  }
+}
+
+// Step 4: Usage
+let defaultUser = User(username: "DefaultUser",
+                             bio: "This is a default bio.")
+
+let user1 = defaultUser.customize(username: "User1",
+                                                    bio: "This is User1's bio.")
+                                                    
